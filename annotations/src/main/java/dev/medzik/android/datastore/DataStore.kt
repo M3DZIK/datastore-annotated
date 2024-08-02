b956457a716ef5179f30b98294cdd3e8235086e9
Oskar Karpiński <me@medzik.dev>
2024-08-02 14:44:23 +0200

    Add documentation to `DataStore` annotation class

--- annotations/src/main/java/dev/medzik/android/datastore/DataStore.kt
+++ annotations/src/main/java/dev/medzik/android/datastore/DataStore.kt
@@ -3,6 +3,10 @@
 
 package dev.medzik.android.datastore
 
+/**
+ * Creates a DataStore for the annotated class.
+ * Requires the class to be also annotated with @Serializable.
+ */
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.CLASS)
 annotation class DataStore(val name: String)
